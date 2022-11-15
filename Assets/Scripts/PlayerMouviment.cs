a532b3a4e666475c6dad81ecd24e289e4aa51ba6
HENRIQUE\Primero <pauloh458@gmail.com>
2022-11-15 20:32:34 +0100

    15/11/2022

--- Assets/Scripts/PlayerMouviment.cs
+++ Assets/Scripts/PlayerMouviment.cs
@@ -8,6 +8,8 @@ public class PlayerMouviment: MonoBehaviour
     Animator anim;
     public float speed = 10;
     public float rotationSpeed = 10;
+    public GameObject bala;
+    public GameObject Boquilla;
 
     void Start()
     {
@@ -21,15 +23,31 @@ public class PlayerMouviment: MonoBehaviour
         if (vertical > 0)
         { 
             rb.AddForce(transform.up * vertical * speed * Time.deltaTime);
-            anim.SetBool("impulsing", true);
+            anim.SetBool("Impulsing", true);
         }
         else
         {
-            anim.SetBool("impulsing", false);
+            anim.SetBool("Impulsing", false);
         }
 
         
         float horizontal = Input.GetAxis("Horizontal");
         transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, horizontal * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject temp = Instantiate(bala, Boquilla.transform.position, transform.rotation);
+            Destroy(temp, 1.5f);
+        }
+
+    }
+        public void Muerte()
+    {
+        GameManager.instance.vidas -= 1;
+        transform.position = new Vector3(0, 0, 0);
+        rb.velocity= new Vector2(0, 0);
+        if (GameManager.instance.vidas <= 0);
+         //Destroy(gameObject);
+
     }
 }
